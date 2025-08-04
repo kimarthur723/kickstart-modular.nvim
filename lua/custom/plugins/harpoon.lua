f16c773a3638f08230e58779c9062fff1b7fcff5
fishe <kimarthur723@gmail.com>
2025-08-04 08:43:00 -0400

    flutter tools

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,35 +1,61 @@
 return {
-  'ThePrimeagen/harpoon',
-  lazy = false,
+  'theprimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    local harpoon = require 'harpoon'
-
-    harpoon:setup {}
-
-    vim.keymap.set('n', '<leader>hl', function()
-      require('harpoon.ui'):toggle_quick_menu(harpoon:list())
-    end, { desc = '[H]arpoon [L]ist' })
-
-    vim.keymap.set('n', '<leader>ha', function()
-      harpoon:list():add()
-    end, { desc = '[H]arpoon [A]dd file' })
-
-    vim.keymap.set('n', '<leader>h1', function()
-      harpoon:list():select(1)
-    end, { desc = '[H]arpoon buffer [1]' })
-
-    vim.keymap.set('n', '<leader>h2', function()
-      harpoon:list():select(2)
-    end, { desc = '[H]arpoon buffer [2]' })
-
-    vim.keymap.set('n', '<leader>h3', function()
-      harpoon:list():select(3)
-    end, { desc = '[H]arpoon buffer [3]' })
-
-    vim.keymap.set('n', '<leader>h4', function()
-      harpoon:list():select(4)
-    end, { desc = '[H]arpoon buffer [4]' })
+    require('harpoon'):setup()
   end,
+
+  keys = {
+    {
+      '<leader>ha',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'harpoon file',
+    },
+    {
+      '<leader>hl',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'harpoon quick menu',
+    },
+    {
+      '<leader>h1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'harpoon to file 1',
+    },
+    {
+      '<leader>h2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'harpoon to file 2',
+    },
+    {
+      '<leader>h3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'harpoon to file 3',
+    },
+    {
+      '<leader>h4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'harpoon to file 4',
+    },
+    {
+      '<leader>h5',
+      function()
+        require('harpoon'):list():select(5)
+      end,
+      desc = 'harpoon to file 5',
+    },
+  },
 }
